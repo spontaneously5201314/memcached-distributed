c66fe053fad59f9054423a4e2d363c39c2de303e
koreyoshi5201314@163.com <hongfei520>
2016-08-29 23:02:43 +0800

    第二次提交

---
+++ src/main/java/com/youxigu/memcached/distributed/algorithm/ConsistentAlgorithm.java
@@ -0,0 +1,11 @@
+package com.youxigu.memcached.distributed.algorithm;
+
+/**
+ * Created by a on 2016/8/29.
+ */
+public class ConsistentAlgorithm extends AlgorithmAdapter {
+    @Override
+    public long hash(String key) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/youxigu/memcached/distributed/algorithm/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Created by a on 2016/8/29.
+ */
+package com.youxigu.memcached.distributed.algorithm;
+
+/*
+* this package is for algorithm to implements hash.
+* 提供一些可以使用的hash算法，常用的放在Default里。
+*
+* */
